0dc05d08a78b500ccf3c6df54508754086099b83
Christian Lopez <32691571+LopezChris@users.noreply.github.com>
2019-04-09 18:33:49 +0000

    Update stop_inference.sh

--- utils/stop_inference.sh
+++ utils/stop_inference.sh
@@ -7,7 +7,7 @@ fi
 if kill $(ps aux | grep '[p]ython run_inference.py' | awk '{print $2}'); then
   echo "Killed inference application"
 else
-  echo "Failed to stop Snapshots application. Is it running?"
+  echo "Failed to stop inference application. Is it running?"
 fi
 
 
